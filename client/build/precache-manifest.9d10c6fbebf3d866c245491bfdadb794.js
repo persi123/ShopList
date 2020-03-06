fd5d0af3da313d67f1de8374f566a3324628f01a
prashant tongra <40728348+persi123@users.noreply.github.com>
2020-03-06 17:47:49 +0530

    deploy hero

---
+++ client/build/precache-manifest.9d10c6fbebf3d866c245491bfdadb794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592f3b16d136bccc96864130e7b167f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c3442145c8b7dc443b4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fbc47d2d0a374d6da466",
+    "url": "/static/css/main.da1b6bf1.chunk.css"
+  },
+  {
+    "revision": "1c3442145c8b7dc443b4",
+    "url": "/static/js/2.f5695e1f.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.f5695e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc47d2d0a374d6da466",
+    "url": "/static/js/main.02574b59.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
